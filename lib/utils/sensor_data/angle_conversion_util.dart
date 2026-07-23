beeb575a17c8a60c56da55eb0a0a385c378b991c
Tom Berger <berger.tom@mail.de>
2026-07-23 17:13:46 +0200

    fix incorrect angle conversion

--- lib/utils/sensor_data/angle_conversion_util.dart
+++ lib/utils/sensor_data/angle_conversion_util.dart
@@ -1,9 +1,10 @@
 // Firmware encodes angle over a -90..+90 degree full scale into a uint16
 // (see rowing_boat SimData::dolle): raw = (angleDeg + 90) / 180 * 65535.
 const double _angleFullScaleDegrees = 180.0;
-const double _angleOffsetDegrees = 90.0;
+const double _angleOffsetDegrees = -180.0;
 const int _uint16Max = 65535;
 
 double convertAngleSensorData(int raw) {
-  return raw / _uint16Max * _angleFullScaleDegrees - _angleOffsetDegrees;
+  //return -180 + raw / _uint16Max * _angleFullScaleDegrees - _angleOffsetDegrees;
+  return -180 + raw * (180 - -180) / _uint16Max;
 }
